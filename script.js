3a9f9c07cb9f4da1113f5bdc077afa405efc882f
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-01-22 18:42:37 -0700

    made button functional by adjusting javascript and html code

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+//const h2 = document.createElement("h2");
+//h2.textContent = "This content added by JavaScript";
+//document.querySelector("body").appendChild(h2);
+
+const inputButton = document.getElementById('puzzle-button');
+
+inputButton.addEventListener('click', function() {
+    alert('IT WORKED');
+});
+
