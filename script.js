c69085e6a0c05755a375e8a47f3b0d062d15958f
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-01-23 03:08:59 -0700

    added javascript to zoom profile image on mouseover and unzoom on mouseleave

--- script.js
+++ script.js
@@ -3,11 +3,22 @@
 //document.querySelector("body").appendChild(h2);
 
 const inputButton = document.getElementById('puzzle-button');
-const mouseoverCaption = document.getElementById('my-pic');
+const mouseoverHighlight = document.getElementById('profile-pic');
 
 inputButton.addEventListener('click', function() {
     alert('Puzzle Solution: ...Rg6+ Kh3, Rg4. White must sacrifice their queen or lose the game immediately.');
 });
+
+mouseoverHighlight.addEventListener('mouseover', function(event){
+    if (event = true) {
+        console.log("ITS HAPPENING");
+        mouseoverHighlight.style.scale = "150%"}        
+});
+
+mouseoverHighlight.addEventListener('mouseleave', function(event){
+    if (event=true) {
+        mouseoverHighlight.style.scale = "100%"}    
+})
 
 
 
