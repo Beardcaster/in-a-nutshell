c7fb3c36a51e1ee2922cc81c593d41c493dea3fb
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-01-23 00:22:48 -0700

    updated image to show information tag when moused-over

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 //document.querySelector("body").appendChild(h2);
 
 const inputButton = document.getElementById('puzzle-button');
+const mouseoverCaption = document.getElementById('my-pic');
 
 inputButton.addEventListener('click', function() {
     alert('Puzzle Solution: ...Rg6+ Kh3, Rg4. White must sacrifice their queen or lose the game immediately.');
 });
+
+
 
