1503b6a35d80e56ad807956c65c23fc7684de4a7
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-02-09 23:45:12 -0700

    revamped site with new knowledge

---
+++ data.js
@@ -0,0 +1,16 @@
+const profileData = {
+    standImg: "https://cdn.imgchest.com/files/g4z9cwr5p72.png",
+    hltImg: "https://cdn.imgchest.com/files/wye3cd6mk4b.png",
+    profileText: "Hi, may name is Jayson and I am learning to develop software. Aside from software development, I like hockey, disc golf, chess and drawing. Thank's for stopping by.",
+    profileImg: "https://cdn.imgchest.com/files/345xc93ov7w.jpg"
+}
+
+const chessData = {
+    standImg: "https://cdn.imgchest.com/files/w7w6cv5ddye.png",
+    hltImg: "https://cdn.imgchest.com/files/k739c6wvq7q.png",
+    chessText: "Puzzle of the week (and likely longer)",
+    chessPuzzleImg: "https://cdn.imgchest.com/files/g4z9cwrpp72.PNG",
+    chessPuzzleSolution: "1...Qg1+, 2. Qf1 Qxf1+, 3. Nd1 Qxd1#",
+    solutionBtnImg: "https://cdn.imgchest.com/files/my8xckmda4l.png",
+    solutionHltImg: "https://imgchest.com/p/vj4jvwke48m#gallery-1"
+}
--- script.js
+++ script.js
@@ -1,23 +1,71 @@
-const inputButton = document.getElementById('puzzle-button');
-const mouseoverHighlight = document.getElementById('profile-pic');
+const loadedText = document.getElementById("visible-text");
+const loadedImage = document.getElementById("visible-image")
+const profButton = document.getElementById("prof-btn");
+const chessButton = document.getElementById("chess-btn");
+const solutionBtn = document.getElementById("solution-btn")
 
-inputButton.addEventListener('click', function() {
-    alert('Puzzle Solution: ...Rg6+ Kh3, Rg4. White must sacrifice their queen or lose the game immediately.');
-});
-
-mouseoverHighlight.addEventListener('mouseover', function(event){
-    if (event = true) {        
-        mouseoverHighlight.style.scale = "110%"}        
-});
-
-mouseoverHighlight.addEventListener('mouseleave', function(event){
-    if (event=true) {
-        mouseoverHighlight.style.scale = "100%"}    
+profButton.addEventListener('mouseover', e => {
+    profButton.src = profileData.hltImg;
 })
 
-/*function displayProfileContent(string){
-        const element = document.createElement("p")
-        element.internalHTML(string);
-        document.body.append(element);
-}*/
+profButton.addEventListener('mouseleave', e => {
+    profButton.src = profileData.standImg;
+})
+
+chessButton.addEventListener('mouseover', e => {
+    chessButton.src = chessData.hltImg;
+})
+
+chessButton.addEventListener('mouseleave', e => {
+    chessButton.src = chessData.standImg
+})
+
+profButton.addEventListener('click', renderProfileContent);
+chessButton.addEventListener('click', renderChessContent);
+
+
+
+function renderProfileContent () {
+
+    profButton.style.scale = "80%"
+    setTimeout(() => profButton.style.scale = "100%", 50)
+
+    loadedText.innerHTML = "";
+    loadedImage.innerHTML = "";
+    solutionBtn.innerHTML = "";
+
+    const p = document.createElement('p')
+    p.innerText = profileData.profileText;
+    loadedText.appendChild(p);
+
+    loadedImage.innerHTML = "";
+    const img = document.createElement("img")
+    img.src = profileData.profileImg;
+    loadedImage.appendChild(img)  
+
+}
+
+function renderChessContent () {    
+
+    chessButton.style.scale = "80%"
+    setTimeout(() => chessButton.style.scale = "100%", 50) 
+
+    loadedText.innerHTML = "";
+    loadedImage.textContent = "";
+    solutionBtn.innerHTML = "";
+
+    const p = document.createElement('p')
+    p.innerText = chessData.chessText;
+    loadedText.appendChild(p);
+
+    const img = document.createElement("img")
+    img.src = chessData.chessPuzzleImg;
+    loadedImage.appendChild(img)
+    
+    const btn = document.createElement("img")
+    btn.src = chessData.solutionBtnImg
+    btn.setAttribute("id","solution")
+    solutionBtn.appendChild(btn);
+}
+    
 
